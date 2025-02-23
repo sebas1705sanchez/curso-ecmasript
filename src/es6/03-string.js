61283bc6138388681f0aa63f41ddfd54ea0444bb
sebas1705sanchez <s.sanchez8@utp.edu.co>
2025-02-23 15:21:06 -0600

    strings

---
+++ src/es6/03-string.js
@@ -0,0 +1,12 @@
+let hello = 'Hello';
+let world = 'world';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// Tamplate literals
+let epicPhrase2 =  `${hello} ${world}`
+console.log(epicPhrase2);
+
+let lorem = `Esto es una frase epica
+y esto es el cambio de linea`;
+console.log(lorem);
