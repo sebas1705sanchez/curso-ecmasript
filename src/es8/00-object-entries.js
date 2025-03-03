607063d85d5973a62f73ad6d7022bea6d86afe7d
sebastian1705 <sebas1705sanchez@gmail.com>
2025-03-03 09:54:18 -0600

    object

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,10 @@
+const countries = {
+    'MX': 'Mexico',
+    'COL': 'Colombia',
+    'PE': 'Peru'
+}
+
+console.log(countries);
+console.log(Object.entries(countries));
+console.log(Object.values(countries));
+console.log(Object.keys(countries));
